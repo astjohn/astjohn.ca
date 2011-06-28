6c22720ee4b6ae93355d0e39b085bc525d6f9e74
Adam St. John <astjohn@gmail.com>
2011-06-28 09:19:26 -0400

    Added refinerycms.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AstjohnCa::Application.config.session_store :cookie_store, :key => '_astjohn.ca_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AstjohnCa::Application.config.session_store :active_record_store
