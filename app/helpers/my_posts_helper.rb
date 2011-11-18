8e37e965aeee39ac7e99850e5013861ca321dfc1
Adam St. John <astjohn@gmail.com>
2011-11-18 10:51:35 -0500

    disqus comments.

--- app/helpers/my_posts_helper.rb
+++ app/helpers/my_posts_helper.rb
@@ -4,12 +4,12 @@ module MyPostsHelper
   def comment_link(post)
     str = ""
     if BlogPost.comments_allowed?
-      if post.comments.any?
-        str = pluralize(post.comments.approved.count,
-                        t('singular', :scope => 'blog.shared.comments'))
-      else
-        str = t('none', :scope => 'blog.shared.comments')
-      end
+#      if post.comments.any?
+#        str = pluralize(post.comments.approved.count,
+#                        t('singular', :scope => 'blog.shared.comments'))
+#      else
+#        str = t('none', :scope => 'blog.shared.comments')
+#      end
       str += image_tag "blog/dialog.png", :alt => "comments", :class => "comments"
       str += link_to str, blog_post_url(post).to_s + "#disqus_thread",
                    {"data-disqus-identifier" => "astjohn.ca_blog_#{post.id}"}
