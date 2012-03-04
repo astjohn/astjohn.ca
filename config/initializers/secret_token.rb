20e57d801c2633b2a44d9567cb625f87eb5d9301
Adam St. John <astjohn@gmail.com>
2012-03-04 09:07:40 -0500

    Clear out rails stuff.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AstjohnCa::Application.config.secret_token = '4c5ee8d71c5f7e971c5db06a5a0a35a04d70deb851f28ea05250fd54982f5e76fb7b9776d0f8eebe23a16d1b7d6c5cab6abf0ca9820463c62e26db0844e1b62b'
