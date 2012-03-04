ce2cc5a73be05cae38d2e6038a5ee66fe9b35aba
Adam St. John <astjohn@gmail.com>
2012-03-04 16:09:45 -0500

    Added mobile styles. everything except sticky footer working.

--- wp-content/themes/astjohn_ca/home_page.php
+++ wp-content/themes/astjohn_ca/home_page.php
@@ -10,10 +10,10 @@ Template Name: Home Page
 
 <div id="splash_wrap">
   <ul id="words">
-    <li class="menu_photo"><a href="#"><div class="menu_photo"></div></a></li>
-    <li class="menu_blog"><a href="#"><div class="menu_blog"></div></a></li>
-    <li class="menu_web"><a href="#"><div class="menu_web"></div></a></li>
-    <li class="menu_travel"><a href="#"><div class="menu_travel"></div></a></li>
+    <li class="menu_photo"><a href="#" title="Photography"><div class="menu_photo"></div></a></li>
+    <li class="menu_blog"><a href="#" title="Learn"><div class="menu_blog"></div></a></li>
+    <li class="menu_web"><a href="#" title="Web Development"><div class="menu_web"></div></a></li>
+    <li class="menu_travel"><a href="#" title="Travel"><div class="menu_travel"></div></a></li>
   </ul>
   <div id="splash_me"></div>
 </div>
