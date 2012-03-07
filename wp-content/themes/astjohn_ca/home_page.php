03371f326dffd014d841f38d6cff55024eb7e74f
Adam St. John <astjohn@gmail.com>
2012-03-06 20:42:17 -0500

    Home page link to blog for now.

--- wp-content/themes/astjohn_ca/home_page.php
+++ wp-content/themes/astjohn_ca/home_page.php
@@ -10,10 +10,10 @@ Template Name: Home Page
 
 <div id="splash_wrap">
   <ul id="words">
-    <li class="menu_photo"><a href="#" title="Photography"><div class="menu_photo"></div></a></li>
-    <li class="menu_blog"><a href="#" title="Learn"><div class="menu_blog"></div></a></li>
-    <li class="menu_web"><a href="#" title="Web Development"><div class="menu_web"></div></a></li>
-    <li class="menu_travel"><a href="#" title="Travel"><div class="menu_travel"></div></a></li>
+    <li class="menu_photo"><a href="http://www.astjohn.ca/blog/" title="Photography"><div class="menu_photo"></div></a></li>
+    <li class="menu_blog"><a href="http://www.astjohn.ca/blog/" title="Learn"><div class="menu_blog"></div></a></li>
+    <li class="menu_web"><a href="http://www.astjohn.ca/blog/" title="Web Development"><div class="menu_web"></div></a></li>
+    <li class="menu_travel"><a href="http://www.astjohn.ca/blog/" title="Travel"><div class="menu_travel"></div></a></li>
   </ul>
   <div id="splash_me"></div>
 </div>
